da0749ba5a50f87aa4dce7c1a2ffbe772d81ed9a
Judith Huynh <judithuynh@gmail.com>
2020-04-10 00:05:10 +0000

    updated wrong URL, and put requestion into a new function, exported it for other files to use

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,22 @@
 const request = require('request');
-const breedNameInput = process.argv[2];
-// console.log(breedNameInput)
-let catUrl = `https://api.thecatapi.com/v1/breeds/seach?q=${breedNameInput}`;
 
 
+const fetchBreedDescription = (breedName, callback) => {
+  const catURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(catURL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length !== 0) { //there is a breed present
+      let desc = data[0].description.trim();  // description of breed, had trailing spaces at the end.
+      callback(null, desc);
+    } else {
+      callback(null, `${breedName} is not a breed!!`);
+    }
+  });
+};
 
 
-
-request((catUrl), (error, response, body) => {
-  
-  if (error) { //when there is an error
-    console.log('Error: ', error);
-  }
-  const data = JSON.parse(body);
-  if (data.length > 0) { //there is a breed present
-    console.log(data[0].description);
-  } else {   //when not a breed
-    console.log(`${breedNameInput} was not found`);
-  }
-// console.log(typeof body) //string => need to converrt that to an object
-});
-
-
-//curl --request GET \
-// --url 'https://api.thecatapi.com/v1/breeds?attach_breed=0' \
-// --header '0c6e1a72-f1d8-4772-b738-b534f6f02941: DEMO-API-KEY'
+module.exports = { fetchBreedDescription };
